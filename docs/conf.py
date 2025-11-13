f52c50df655d92f3330ee6c42795b4d831a54d88
Daniel Pasin <66703677+dpasin@users.noreply.github.com>
2025-11-13 16:50:26 +1100

    Update docs

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath('..'))  # so Sphinx can import amberNPS
 project = 'amberNPS'
 copyright = '2025, Daniel Pasin'
 author = 'Daniel Pasin'
-release = '0.1.4'
+release = '0.1.6'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
